cb5f593c21b283c6fd20bf287110fc7bcb3cf539
Sumudu Priyabhashinee <59961195+SumuduPriyabhashinee@users.noreply.github.com>
2021-10-16 20:56:42 +0530

    Update 2021_10_16_151255_create_items_table.php

--- database/migrations/2021_10_16_151255_create_items_table.php
+++ database/migrations/2021_10_16_151255_create_items_table.php
@@ -15,6 +15,10 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal(15,2)('price');
+            $table->int('amount');
+            $table->int('reorder_level');
             $table->timestamps();
         });
     }
