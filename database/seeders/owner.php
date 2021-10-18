72f25bf90fa4c23fef20a46a749f67516ad34421
Sumudu Priyabhashinee <59961195+SumuduPriyabhashinee@users.noreply.github.com>
2021-10-18 16:28:34 +0530

    add universal user

---
+++ database/seeders/owner.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class owner extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('users')->insert(
+        //     ['name' => 'owner',
+        //     'email' => 'owner@gmail.com',
+        //     'password' => Hash::make('123456'),
+        // ]
+        // );
+        $user = User::create([
+            'name'=>'owner',
+            'email'=>'owner@gmail.com',
+            'password' => Hash::make('123456')
+        ]);
+        
+        $user->attachRole('owner');
+    }
+}
